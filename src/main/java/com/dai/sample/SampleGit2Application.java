5c0d2d5ab4d45a2b849a06c82fd1bd2ff0652112
daice <daice@DESKTOP-0O8J0UF>
2017-11-27 14:46:23 +0800

    初始导入

---
+++ src/main/java/com/dai/sample/SampleGit2Application.java
@@ -0,0 +1,12 @@
+package com.dai.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGit2Application.class, args);
+	}
+}
