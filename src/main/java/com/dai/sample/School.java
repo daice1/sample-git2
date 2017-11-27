78c58f8cbdae40a02fede2a57f501e96e46935a6
daice <daice@DESKTOP-0O8J0UF>
2017-11-27 15:19:01 +0800

    添加学校类

---
+++ src/main/java/com/dai/sample/School.java
@@ -0,0 +1,16 @@
+package com.dai.sample;
+
+public class School {
+
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
